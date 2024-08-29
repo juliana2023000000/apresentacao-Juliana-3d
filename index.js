4807985ba530eb7ad682d26be99671f49ebd643c
Juliana <138030471+juliana2023000000@users.noreply.github.com>
2024-08-29 01:27:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fistName = "Juliana", lastName = " Golinelli";
+//const lastName = " Golinelli";
+const idade = 18;
+const altura = 1.52;
+const peso = 69.2;
+const cidadeNatal = "Curitiba";
+console.log("Hello, World! " + fistName + lastName + ". Your age is: " + idade + "?");
+console.log("and your height is: " + altura + ", and your weight is: " + peso);
+console.log("Your hometown is: " + cidadeNatal)
